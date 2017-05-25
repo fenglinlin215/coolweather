7f81b326a9d26fba7e41dc1dec774a42aac06969
C515 <C515>
2017-05-25 14:22:13 +0800

    加入创建数据库和表的各项配置

---
+++ app/src/main/java/com/example/fll/coolweather/db/County.java
@@ -0,0 +1,47 @@
+package com.example.fll.coolweather.db;
+
+/**
+ * Created by C515 on 2017/5/25.
+ */
+public class County extends DataSupport{
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+}
